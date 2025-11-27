986f1366d0636a6f418bf72e8de1eaf3a9eff145
dbilalfaiznew-hash <dbilalfaiznew@gmail.com>
2025-11-27 15:17:04 -0800

    Update Viewver.cc

--- Viewver.cc
+++ Viewver.cc
@@ -1,4 +1,5 @@
 export module Viewver;
+import xwindow;
 import <iostream>;
 import <string>;
 import <vector>;
@@ -10,6 +11,7 @@ export class Viewver {
   // Graphics will be implemented using X11 in the future
   int width;
   int height;
+  Xwindow window;
 public:
   Viewver(int w = 11, int h = 18);
   /**
@@ -17,4 +19,5 @@ public:
    * @param game The game instance to draw.
   */
   void drawGrid(const Board &p1, const Board &p2, const Level* level_p1, const Level* level_p2) const;
+  void drawGraphics(const Board&, const Board&, const Level* level_p1, const Level* level_p2);
 };
